224861ad584cf703d4300d24cf0c5ed7b0562aa6
Swai-D <davyswai@gmail.com>
2022-01-31 22:35:34 +0300

    post-image-update

--- resources/views/LayoutBladeFiles/mini-device-bottom-nav-layout.blade.php
+++ resources/views/LayoutBladeFiles/mini-device-bottom-nav-layout.blade.php
@@ -272,6 +272,135 @@
 
 </div>
 <!-- /BAR ACTIONS -->
+@elseif(Auth::user()->user_ocupation == 'Mashine_Za_Kilimo')
+<!-- BAR ACTIONS -->
+<div class="bar-actions">
+  <!-- ACTION LIST -->
+  <div class="action-list dark">
+    <!-- ACTION LIST ITEM -->
+    <a class="action-list-item " href="/kilimofy/Muuzaji-Wa-Mashine-Za-Kilimo/home-page">
+      <!-- ACTION LIST ITEM ICON -->
+      <svg class="action-list-item-icon icon-item" @yield('mini-user-fundi-active')>
+        <use xlink:href="#svg-item"></use>
+      </svg>
+      <!-- /ACTION LIST ITEM ICON -->
+    </a>
+    <!-- /ACTION LIST ITEM -->
+
+    <!-- ACTION LIST ITEM -->
+    <a class="action-list-item " href="/kilimofy/Group/Group-Lists">
+      <!-- ACTION LIST ITEM ICON -->
+      <svg class="action-list-item-icon icon-group" @yield('mini-user-chat-menu')>
+        <use xlink:href="#svg-group"></use>
+      </svg>
+      <!-- /ACTION LIST ITEM ICON -->
+    </a>
+    <!-- /ACTION LIST ITEM -->
+
+    <!-- ACTION LIST ITEM -->
+    <a class="action-list-item " href="/kilimofy/Group/Group-Lists">
+      <!-- ACTION LIST ITEM ICON -->
+      <svg class="action-list-item-icon icon-magnifying-glass" @yield('mini-user-chat-menu')>
+        <use xlink:href="#svg-magnifying-glass"></use>
+      </svg>
+      <!-- /ACTION LIST ITEM ICON -->
+    </a>
+    <!-- /ACTION LIST ITEM -->
+
+
+    <!-- ACTION LIST ITEM -->
+    <a class="action-list-item " href="/Kilimofy-Messenger">
+      <!-- ACTION LIST ITEM ICON -->
+      <svg class="action-list-item-icon icon-messages" @yield('mini-user-forum-active')>
+        <use xlink:href="#svg-messages"></use>
+      </svg>
+      <!-- /ACTION LIST ITEM ICON -->
+    </a>
+    <!-- /ACTION LIST ITEM -->
+
+
+
+    <!-- ACTION LIST ITEM -->
+    <a class="action-list-item " href="/kilimofy/UserAccount/about_user_page/{{Auth::user()->id}}-about-{{Auth::user()->name}}-of-kilimofy">
+      <!-- ACTION LIST ITEM ICON -->
+      <svg class="action-list-item-icon icon-profile" @yield('mini-user-profile-active')>
+        <use xlink:href="#svg-profile"></use>
+      </svg>
+      <!-- /ACTION LIST ITEM ICON -->
+    </a>
+    <!-- /ACTION LIST ITEM -->
+
+      </div>
+      <!-- /ACTION LIST -->
+
+</div>
+<!-- /BAR ACTIONS -->
+
+@elseif(Auth::user()->user_ocupation == 'Mtaalam')
+<!-- BAR ACTIONS -->
+<div class="bar-actions">
+  <!-- ACTION LIST -->
+  <div class="action-list dark">
+    <!-- ACTION LIST ITEM -->
+    <a class="action-list-item " href="/kilimofy/Muuzaji-Wa-Mashine-Za-Kilimo/home-page">
+      <!-- ACTION LIST ITEM ICON -->
+      <svg class="action-list-item-icon icon-item" @yield('mini-user-fundi-active')>
+        <use xlink:href="#svg-item"></use>
+      </svg>
+      <!-- /ACTION LIST ITEM ICON -->
+    </a>
+    <!-- /ACTION LIST ITEM -->
+
+    <!-- ACTION LIST ITEM -->
+    <a class="action-list-item " href="/kilimofy/Group/Group-Lists">
+      <!-- ACTION LIST ITEM ICON -->
+      <svg class="action-list-item-icon icon-group" @yield('mini-user-chat-menu')>
+        <use xlink:href="#svg-group"></use>
+      </svg>
+      <!-- /ACTION LIST ITEM ICON -->
+    </a>
+    <!-- /ACTION LIST ITEM -->
+
+    <!-- ACTION LIST ITEM -->
+    <a class="action-list-item " href="/kilimofy/Group/Group-Lists">
+      <!-- ACTION LIST ITEM ICON -->
+      <svg class="action-list-item-icon icon-magnifying-glass" @yield('mini-user-chat-menu')>
+        <use xlink:href="#svg-magnifying-glass"></use>
+      </svg>
+      <!-- /ACTION LIST ITEM ICON -->
+    </a>
+    <!-- /ACTION LIST ITEM -->
+
+
+    <!-- ACTION LIST ITEM -->
+    <a class="action-list-item " href="/Kilimofy-Messenger">
+      <!-- ACTION LIST ITEM ICON -->
+      <svg class="action-list-item-icon icon-messages" @yield('mini-user-forum-active')>
+        <use xlink:href="#svg-messages"></use>
+      </svg>
+      <!-- /ACTION LIST ITEM ICON -->
+    </a>
+    <!-- /ACTION LIST ITEM -->
+
+
+
+    <!-- ACTION LIST ITEM -->
+    <a class="action-list-item " href="/kilimofy/UserAccount/about_user_page/{{Auth::user()->id}}-about-{{Auth::user()->name}}-of-kilimofy">
+      <!-- ACTION LIST ITEM ICON -->
+      <svg class="action-list-item-icon icon-profile" @yield('mini-user-profile-active')>
+        <use xlink:href="#svg-profile"></use>
+      </svg>
+      <!-- /ACTION LIST ITEM ICON -->
+    </a>
+    <!-- /ACTION LIST ITEM -->
+
+      </div>
+      <!-- /ACTION LIST -->
+
+</div>
+<!-- /BAR ACTIONS -->
+
+
 @elseif(Auth::user()->user_ocupation == 'Fundi')
 <!-- BAR ACTIONS -->
 <div class="bar-actions">
