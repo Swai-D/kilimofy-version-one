88bb5c161052037e5ccc2b425cef75ff1a82ea26
Swai-D <davyswai@gmail.com>
2022-04-15 08:59:40 +0300

    user-profile nav-update

--- resources/views/LayoutBladeFiles/user-profile-header.blade.php
+++ resources/views/LayoutBladeFiles/user-profile-header.blade.php
@@ -60,7 +60,7 @@
         <!-- USER AVATAR CONTENT -->
         <div class="user-avatar-content">
           <!-- HEXAGON -->
-          <div class="hexagon-image-82-90" data-src="/Uploads/avatars/{{$user->avatar}}"></div>
+          <div class="hexagon-image-82-90" data-src="{{$user->avatar}}"></div>
           <!-- /HEXAGON -->
         </div>
         <!-- /USER AVATAR CONTENT -->
